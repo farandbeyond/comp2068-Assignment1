063168e508842ed1b3354d9a9bb63be9d8c6f6b2
farandbeyond <connorjh4@gmail.com>
2016-10-05 15:18:23 -0400

    3rd commit
    Finished work on the navBar, used an argument to control which link was
    'class "active"'
    added an 'aboutme' page, WIP
    slight css adjustments

---
+++ routes/aboutme.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Connor on 2016-10-05.
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', {
+        pageNum: 2,
+        title: 'About Me'
+    });
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    pageNum: 1,
+    title: 'Homepage'
+  });
 });
 
 module.exports = router;
