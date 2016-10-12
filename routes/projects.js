9564a87ba92cac474c4d7d109f41fb615d316f64
farandbeyond <connorjh4@gmail.com>
2016-10-12 14:52:42 -0400

    Added language to projects page table
    -as above

--- routes/projects.js
+++ routes/projects.js
@@ -10,12 +10,13 @@ router.get('/', function(req, res, next) {
     var projDescription = ['A turn based RPG','Remake of Blackwind 1.0','Remake of Blackwind 2.0','A grid based puzzle game'];
     var projSolo = ['Self, and 1 other','Mostly solo','Self, and 1 other','Solo'];
     var projInProgress = ['Ended at demo','Ended at demo','In progress; On Hold','In progress'];
+    var projLang = ['Java','Java','Java','Java'];
 
 
     res.render('projects', {
         pageNum: 3,
         title: 'My Projects',
-        projects: [projNames,projDescription,projSolo,projInProgress]
+        projects: [projNames,projDescription,projSolo,projInProgress,projLang]
     });
 });
 
